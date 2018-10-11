56d7b284b43f3ea6b08f609f471cdfed1544b7e9
John <53950451@qq.com>
2018-10-11 18:35:37 +0800

    tcp listen

--- src/main/main.go
+++ src/main/main.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"fmt"
-	"tools"
+	"net"
 )
 
-func main() {
-	tools.DoEcho()
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	var buf = make([]byte, 1024)
 
-	fmt.Println("Hello World")
+	fmt.Println(conn.RemoteAddr())
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn read error:", err)
+			return
+		}
+
+		fmt.Printf("read %d bytes, content is %s\n", n, string(buf[:n]))
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":8001")
+	if err != nil {
+		fmt.Println("listen error:", err)
+		return
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			return
+		}
+
+		go handleConn(conn)
+	}
 }
